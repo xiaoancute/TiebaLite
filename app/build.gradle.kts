d25767da7ae27dc3cb555942160b6a7099299f82
xiaoancute <xiaoan114514qwq@outlook.com>
2026-06-24 07:42:43 +0800

    chore: bump version to beta 4.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 391013
-        versionName = "4.0.0 Beta 4.18"
+        versionCode = 391014
+        versionName = "4.0.0 Beta 4.19"
         // Configure custom runner to set up the Hilt test application
         testInstrumentationRunner = "$applicationId.TbLiteTestRunner"
         vectorDrawables {
