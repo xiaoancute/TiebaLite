a6e822fdbc8a640d7885d5d759b2e66d79c9cfe9
xiaoancute <xiaoan114514qwq@outlook.com>
2026-07-22 06:43:13 +0800

    fix: restore getDislikeListFlow on OfficialProtobufTiebaApi

--- app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/interfaces/OfficialProtobufTiebaApi.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/interfaces/OfficialProtobufTiebaApi.kt
@@ -8,6 +8,7 @@ import com.huanchengfly.tieba.post.api.models.protos.forumRuleDetail.ForumRuleDe
 import com.huanchengfly.tieba.post.api.models.protos.frsPage.FrsPageResponse
 import com.huanchengfly.tieba.post.api.models.protos.GeneralTabList.GeneralTabListResponse
 import com.huanchengfly.tieba.post.api.models.protos.getBawuInfo.GetBawuInfoResponse
+import com.huanchengfly.tieba.post.api.models.protos.getDislikeList.GetDislikeListResponse
 import com.huanchengfly.tieba.post.api.models.protos.getForumDetail.GetForumDetailResponse
 import com.huanchengfly.tieba.post.api.models.protos.getHistoryForum.GetHistoryForumResponse
 import com.huanchengfly.tieba.post.api.models.protos.getLevelInfo.GetLevelInfoResponse
@@ -44,6 +45,11 @@ interface OfficialProtobufTiebaApi {
     fun hotThreadListFlow(
         @Body body: MyMultipartBody,
     ): Flow<HotThreadListResponse>
+
+    @POST("/c/u/user/getDislikeList?cmd=309692")
+    fun getDislikeListFlow(
+        @Body body: MyMultipartBody,
+    ): Flow<GetDislikeListResponse>
 
     @POST("/c/f/recommend/topicList?cmd=309289")
     fun topicListFlow(
